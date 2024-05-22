50876d7bdfee466c707ce97f7547776cf13a1208
Zhiyuan Yang <Zhiyuan.Y@arbin.com>
2024-05-22 14:57:28 -0500

    implemented only checkboxes of latest files

--- ACBControlHelper/ChooseFile.cs
+++ ACBControlHelper/ChooseFile.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Report;
 using Report.Util;
+using System.Globalization;
 
 namespace ACBControlHelper
 {
@@ -60,7 +61,7 @@ namespace ACBControlHelper
             {
                 string fileName = Path.GetFileName(fullName);
                 CheckedListBox_Files.Items.Add(fileName);
-                CheckedListBox_Files.SetItemChecked(i++, true);
+                // CheckedListBox_Files.SetItemChecked(i++, true);
             }
 
             ArrangeCheckListBox();
@@ -71,11 +72,53 @@ namespace ACBControlHelper
 
 
         //Dominic
+        //only check the boxes of the latest files 
         private void ArrangeCheckListBox()
         {
+            Dictionary<string,string> latestFiles = new Dictionary<string, string>(); 
+            foreach (var item in CheckedListBox_Files.Items)
+            {
+                if (item != null) 
+                {
+                    string fileName = item.ToString();
+                    string range = ExtractTestSetup(fileName); //  extract range from the file name
+                    DateTime fileDateTime = ExtractDateTime(fileName); // extract date/time from the file name
 
+                    if (!latestFiles.ContainsKey(range) || fileDateTime > ExtractDateTime(latestFiles[range]))
+                    {
+                        latestFiles[range] = fileName;
+                    }
+                }
+            }
+
+            CheckboxLatestFiles(latestFiles); 
         }
-        //
+
+        private void CheckboxLatestFiles(Dictionary<string, string> latestFiles)
+        {
+            foreach (var fileName in latestFiles.Values)
+            {
+                int index = CheckedListBox_Files.Items.IndexOf(fileName);
+                if (index != -1)
+                {
+                    CheckedListBox_Files.SetItemChecked(index, true);
+                }
+            }
+        }
+        private string ExtractTestSetup(string filename)
+        {
+            string[] parts = filename.Split('_'); 
+            return parts[0]; 
+        }
+
+        private DateTime ExtractDateTime(string filename)
+        {
+            string time_s = filename.Split('_').Last(); 
+            time_s = time_s.Split('.').First(); 
+            return DateTime.ParseExact(time_s, "yyyyMMddHHmm", CultureInfo.InvariantCulture); 
+        }
+
+        // 
 
         private static string GetLastFolderName(string path)
         {
